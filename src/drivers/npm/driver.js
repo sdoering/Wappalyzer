67c787fece98b4d22d31298a3367c9aad929a485
Sven Döring <sdoering@users.noreply.github.com>
2017-06-27 11:46:11 +0200

    Update driver.js
    Changed browser agent to prevent blocking

--- src/drivers/npm/driver.js
+++ src/drivers/npm/driver.js
@@ -139,7 +139,8 @@
 				page = require('webpage').create();
 
 				page.settings.loadImages      = false;
-				page.settings.userAgent       = 'Mozilla/5.0 (compatible; Wappalyzer; +https://github.com/AliasIO/Wappalyzer)';
+				// page.settings.userAgent       = 'Mozilla/5.0 (compatible; Wappalyzer; +https://github.com/AliasIO/Wappalyzer)';
+				page.settings.userAgent       = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
 				page.settings.resourceTimeout = resourceTimeout;
 
 				page.onError = function(message) {
